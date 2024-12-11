659a89162ef128c5127de25c5f9018d7355f8b68
bourdima <bourdima@D251-S04>
2024-12-11 11:16:18 +0100

    correcitf

--- Survival Island/joueur/NavireCaracteristique.cs
+++ Survival Island/joueur/NavireCaracteristique.cs
@@ -11,7 +11,7 @@ namespace Survival_Island.joueur
         public double vie { get; set; } = 100;
         public double vieMax { get; set; } = 100;
 
-        public double vitesse { get; set; } = 10;
+        public double vitesse { get; set; } = 5;
         public double degat { get; set; } = 10;
 
         public double tempsRechargementCanon { get; set; } = 1;
