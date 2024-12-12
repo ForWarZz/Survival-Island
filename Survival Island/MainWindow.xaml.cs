fb45ee0a69c428b53ea7bbf274e0b4d7cc205e60
battigm <battigm@D350-S20>
2024-12-12 15:51:59 +0100

    Reorganisation methodes

--- Survival Island/MainWindow.xaml.cs
+++ Survival Island/MainWindow.xaml.cs
@@ -1,31 +1,20 @@
-﻿using Survival_Island.carte;
-using Survival_Island.carte.objets;
+﻿using Survival_Island.carte.objets;
+using Survival_Island.carte;
 using Survival_Island.joueur;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media;
 using System.Windows.Threading;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Windows;
 
 namespace Survival_Island
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
     public partial class MainWindow : Window
     {
-        private int NOMBRE_IMAGE_MER, IM_MER_LARG, IM_MER_HAUT;
-
         private Image[] mer;
 
         private BitmapImage bitmapMer, bitmapTresor;
@@ -40,9 +29,6 @@ namespace Survival_Island
 
         private Random random = new Random();
 
-/*        private List<Item> listeItem = new List<Item>();
-        private List<Ennemi> listeEnnemis;*/
-
         private List<Boulet> boulets;
         private List<Objet> objetsBonus;
         private Obstacle[] obstacles;
@@ -50,13 +36,72 @@ namespace Survival_Island
         public MainWindow()
         {
             InitializeComponent();
-
             InitBitmaps();
             InitCarte();
         }
 
+        private void InitBitmaps()
+        {
+            bitmapMer = new BitmapImage(new Uri(Chemin.IMAGE_MER));
+            bitmapRochers = new BitmapImage[]
+            {
+                new BitmapImage(new Uri(Chemin.IMAGE_ROCHER1)),
+                new BitmapImage(new Uri(Chemin.IMAGE_ROCHER2)),
+                new BitmapImage(new Uri(Chemin.IMAGE_ROCHER3)),
+            };
+            bitmapTresor = new BitmapImage(new Uri(Chemin.IMAGE_TRESOR));
+        }
+
+        private void InitCarte()
+        {
+            mer = new Image[Constante.NOMBRE_CARREAUX_MER];
+            carte.Width = mer.Length * bitmapMer.Width;
+            carte.Height = mer.Length * bitmapMer.Height;
+
+            // Initialisation de la mer en fond
+            for (int i = 0; i < mer.Length; i++)
+            {
+                for (int j = 0; j < mer.Length; j++)
+                {
+                    mer[i] = new Image();
+                    mer[i].Source = bitmapMer;
+                    mer[i].Width = bitmapMer.Width;
+                    mer[i].Height = bitmapMer.Height;
+
+                    Canvas.SetLeft(mer[i], j * bitmapMer.Width);
+                    Canvas.SetTop(mer[i], i * bitmapMer.Height);
+
+                    carte.Children.Add(mer[i]);
+                }
+            }
+        }
+        private void InitBoucleJeu()
+        {
+            CompositionTarget.Rendering += Jeu;
+        }
+
+        private void Jeu(object? sender, EventArgs e)
+        {
+            CheckDeplacement();
+            DeplacerBoulets();
+
+            if (joueur.canonActif)
+                joueur.TirerBoulet();
+        }
+
+        private void CheckDeplacement()
+        {
+            double vitesse = joueur.caracteristique.vitesse;
+
+            if (joueur.deplacement)
+            {
+                Vector orientation = joueur.orientation;
+                joueur.DeplaceBateau(orientation.X * vitesse, orientation.Y * vitesse);
+            }
+        }
+
         private void InitBonusMinuteur()
-        { 
+        {
             objetBonusMinuteur = new DispatcherTimer();
             objetBonusMinuteur.Interval = TimeSpan.FromMinutes(1);
             objetBonusMinuteur.Tick += GenererBonus;
@@ -72,7 +117,6 @@ namespace Survival_Island
                 bool positionValide = true;
                 double posX, posY;
 
-                // On check la position par rapport aux autres elements
                 do
                 {
                     posX = random.Next(0, (int)(carte.Width - bitmapTresor.Width));
@@ -80,7 +124,6 @@ namespace Survival_Island
 
                     Rect rect = new Rect(posX, posY, bitmapTresor.Width, bitmapTresor.Height);
 
-                    // Vérification des collisions avec l'île et les autres obstacles/bonus
                     positionValide = CheckPosition(rect);
                 } while (!positionValide);
 
@@ -109,24 +152,19 @@ namespace Survival_Island
                 rocher.Height = randomRocher.Height * (0.5 + random.NextDouble());
                 rocher.RenderTransform = new RotateTransform(random.Next(0, 361), rocher.Width / 2, rocher.Height / 2);
 
-                Console.WriteLine("Width : " + rocher.Width + ", Height: " + rocher.Height);
-
                 double posX, posY;
                 bool positionValide;
 
-                // Vérification de la position
                 do
-                { 
+                {
                     posX = random.Next(0, (int)(carte.Width - rocher.Width));
                     posY = random.Next(0, (int)(carte.Width - rocher.Height));
 
                     Rect rect = new Rect(posX, posY, rocher.Width, rocher.Height);
 
-                    // Vérification des collisions avec l'île et les autres obstacles
                     positionValide = CheckPosition(rect);
                 } while (!positionValide);
 
-                // Création et ajout de l'obstacle à la carte
                 Obstacle obstacle = new Obstacle(carte, rocher);
                 obstacle.Apparaitre(posX, posY);
 
@@ -136,13 +174,11 @@ namespace Survival_Island
 
         private bool CheckPosition(Rect rect)
         {
-            // Vérification si le rectangle entre en collision avec l'île
             if (ile.EnCollisionAvec(rect))
             {
                 return false;
             }
 
-            // Vérification si le rectangle entre en collision avec d'autres obstacles
             foreach (Obstacle obstacleDejaPresent in obstacles)
             {
                 if (obstacleDejaPresent != null && obstacleDejaPresent.EnCollisionAvec(rect))
@@ -151,7 +187,6 @@ namespace Survival_Island
                 }
             }
 
-            // Vérification si le rectangle entre en collision avec des objet bonus
             foreach (Objet objetsBonusDejaPresent in objetsBonus)
             {
                 if (objetsBonusDejaPresent != null && objetsBonusDejaPresent.EnCollisionAvec(rect))
@@ -163,156 +198,20 @@ namespace Survival_Island
             return true;
         }
 
-        private void InitBoucleJeu()
-        {
-            CompositionTarget.Rendering += Jeu;
-        }
-
-        private void Jeu(object? sender, EventArgs e)
-        {
-            CheckDeplacement();
-            DeplacerBoulets();
-
-            if (joueur.canonActif)
-                joueur.TirerBoulet();
-        }
-
-        private void CheckDeplacement()
-        {
-            double vitesse = joueur.caracteristique.vitesse;
-
-            if (joueur.deplacement)
-            {
-                Vector orientation = joueur.orientation;
-                joueur.DeplaceBateau(orientation.X * vitesse, orientation.Y * vitesse);
-            }
-        }
-
-        private void InitBitmaps()
-        {
-            bitmapMer = new BitmapImage(new Uri(Chemin.IMAGE_MER));
-            bitmapRochers = [
-                    new BitmapImage(new Uri(Chemin.IMAGE_ROCHER1)),
-                    new BitmapImage(new Uri(Chemin.IMAGE_ROCHER2)),
-                    new BitmapImage(new Uri(Chemin.IMAGE_ROCHER3)),
-                ];
-
-            bitmapTresor = new BitmapImage(new Uri(Chemin.IMAGE_TRESOR));
-        }
-
-        private void InitCarte()
-        {
-            mer = new Image[Constante.NOMBRE_CARREAUX_MER];
-            carte.Width = mer.Length * bitmapMer.Width;
-            carte.Height = mer.Length * bitmapMer.Height;
-
-            /// Initialisation de la mer en fond
-            for (int i = 0; i < mer.Length; i++)
-            {
-                for (int j = 0; j < mer.Length; j++)
-                {
-                    mer[i] = new Image();
-                    mer[i].Source = bitmapMer;
-                    mer[i].Width = bitmapMer.Width;
-                    mer[i].Height = bitmapMer.Height;
-
-                    Canvas.SetLeft(mer[i], j * bitmapMer.Width);
-                    Canvas.SetTop(mer[i], i * bitmapMer.Height);
-
-                    carte.Children.Add(mer[i]);
-                }
-            }
-        }
-
-        private void LancerJeu()
-        {
-            hudJoueur.Visibility = Visibility.Visible;
-
-            boulets = new List<Boulet>();
-            objetsBonus = new List<Objet>();
-
-            ile = new Ile(carte, progressVieIle, txtVieIle);
-            ile.Apparaitre();
-
-            joueur = new Joueur(carte, this, boulets);
-            joueur.Apparaitre();
-
-            InitRochers();
-            InitBonusMinuteur();
-
-            InitBoucleJeu();
-
-
-            //Fonction de test des items.
-
-            //AjoutItems(listeItem, 70, [Chemin.IMAGE_TRESOR], 0, 4000, 0, 4000, 20, 70, false);
-            //AjoutItems(listeItem, 70, [Chemin.IMAGE_ROCHER1, Chemin.IMAGE_ROCHER2, Chemin.IMAGE_ROCHER3], 0, 4000, 0, 4000, 50, 200, true);
-            /*InitEnemies();*/
-        }
-
-/*        private void DeplaceCamera(double deltaX, double deltaY)
-        {
-            cameraX += deltaX;
-            cameraY += deltaY;
-
-            // Restreindre les limites de la caméra
-            cameraX = Math.Max(0, Math.Min(cameraX, carte.Width - Fenetre.ActualWidth));
-            cameraY = Math.Max(0, Math.Min(cameraY, carte.Height - Fenetre.ActualHeight));
-
-            double bateauX = Canvas.GetLeft(joueur.canvaElement);
-            double bateauY = Canvas.GetTop(joueur.canvaElement);
-
-            // Appliquer le décalage au Canvas (monde)
-            Canvas.SetLeft(carte, -cameraX);
-            Canvas.SetTop(carte, -cameraY);
-
-            joueur.DeplaceBateau(deltaX, deltaY);
-        }*/
-
-/*        private void InitCamera()
-        {
-            cameraX = carte.Width / 2 - Fenetre.ActualWidth / 2;
-            cameraY = carte.Height / 2 - Fenetre.ActualHeight / 2;
-
-            Canvas.SetLeft(carte, -cameraX);
-            Canvas.SetTop(carte, -cameraY);
-
-            Console.WriteLine("Camera X: " + cameraX + ", Camera Y: " + cameraY);
-        }*/
-
-        /*
-                private void DeplaceMonde(double x, double y)
-                {
-                    foreach (UIElement canvaElement in carte.Children)
-                    {
-                        // Ignorer le joueur et autres éléments ne devant pas bouger
-                        if (canvaElement == joueur.canvaElement)
-                            continue;
-
-                        double positionX = Canvas.GetLeft(canvaElement);
-                        Canvas.SetLeft(canvaElement, positionX + x);
-
-                        double positionY = Canvas.GetTop(canvaElement);
-                        Canvas.SetTop(canvaElement, positionY + y);
-                    }
-                }*/
-
         private void DeplacerBoulets()
         {
-            // On parcourt la liste des boulets à l'envers pour pouvoir supprimer des éléments
             for (int i = 0; i < boulets.Count; i++)
             {
                 Boulet boulet = boulets[i];
                 double bouletX = Canvas.GetLeft(boulet.canvaElement);
                 double bouletY = Canvas.GetTop(boulet.canvaElement);
 
-                bouletX += boulet.direction.X * Constante.VITESSE_BOULET; // Vitesse du boulet
+                bouletX += boulet.direction.X * Constante.VITESSE_BOULET;
                 bouletY += boulet.direction.Y * Constante.VITESSE_BOULET;
 
                 Canvas.SetLeft(boulet.canvaElement, bouletX);
                 Canvas.SetTop(boulet.canvaElement, bouletY);
 
-                // Supprimer le boulet si hors écran
                 if (bouletX < 0 || bouletY < 0 ||
                     bouletX > carte.Width + boulet.canvaElement.Width || bouletY > carte.Height + boulet.canvaElement.Width)
                 {
@@ -323,33 +222,25 @@ namespace Survival_Island
 
             if (joueur.tempsDernierBoulet > 0)
             {
-                joueur.tempsDernierBoulet -= 1.0 / 60.0;   // On suppose 60 FPS, pour convertir
+                joueur.tempsDernierBoulet -= 1.0 / 60.0;
             }
-
         }
 
         private void Fenetre_KeyUp(object sender, KeyEventArgs e)
         {
-            if (jouer)
+            if (jouer && e.Key == Key.Z)
             {
-                if (e.Key == Key.Z)
-                {
-                    joueur.deplacement = false;
-                }
+                joueur.deplacement = false;
             }
         }
 
         private void Fenetre_KeyDown(object sender, KeyEventArgs e)
         {
-            if (jouer)
+            if (jouer && e.Key == Key.Z)
             {
-                if (e.Key == Key.Z)
-                {
-                    joueur.deplacement = true;
-                }
+                joueur.deplacement = true;
             }
         }
-
 
         private void Fenetre_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -365,13 +256,28 @@ namespace Survival_Island
 
         private void Fenetre_MouseMove(object sender, MouseEventArgs e)
         {
-
             if (jouer)
-            { 
+            {
                 Point positionSouris = e.GetPosition(carte);
                 joueur.UpdateOrientation(positionSouris);
             }
+        }
+        private void LancerJeu()
+        {
+            hudJoueur.Visibility = Visibility.Visible;
 
+            boulets = new List<Boulet>();
+            objetsBonus = new List<Objet>();
+
+            ile = new Ile(carte, progressVieIle, txtVieIle);
+            ile.Apparaitre();
+
+            joueur = new Joueur(carte, this, boulets);
+            joueur.Apparaitre();
+
+            InitRochers();
+            InitBonusMinuteur();
+            InitBoucleJeu();
         }
 
         private void btnFermerJeu_Click(object sender, RoutedEventArgs e)
